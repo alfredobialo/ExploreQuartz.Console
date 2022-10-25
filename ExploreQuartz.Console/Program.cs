d8e74c498b7d6a944d30249a979596d29fe9d2ff
Alfred Obialo <alfredcsdinc@gmail.com>
2022-10-25 16:21:50 +0100

    Basic configuration for a Schduler

--- ExploreQuartz.Console/Program.cs
+++ ExploreQuartz.Console/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ExploreQuartz.Lib;
+
 Console.WriteLine("Hello, World!");
+
+SimpleScheduler scheduler =  new SimpleScheduler();
+var sch = await scheduler.CreateSimpleScheduler();
+await sch.Start(CancellationToken.None);
